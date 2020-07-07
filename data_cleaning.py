01d235921455177dcd00ab743a169848269434c3
Ilham Akbar <ilhamije@gmail.com>
2020-07-07 15:52:46 +0700

    added cleaning for odometer

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,9 +2,13 @@ import pandas as pd
 
 df = pd.read_csv('carprice_scraper/output/carmudi-dataset.csv') 
 
+df['brand'] = df['brand'].apply(lambda x: x.strip('\"\n '))
 df['location'] = df['location'].apply(lambda x: x.strip('\n'))
-df['odometer'] = df['odometer'].apply(lambda x: x.strip('Km'))
 df['price'] = df['price'].apply(lambda x: x.strip('Juta'))
+
+df['odometer'] = df['odometer'].apply(lambda x: x.strip(' Km'))
+df['odometer'] = df['odometer'].str.replace(',', '').astype(float)
+df['odometer'] = pd.to_numeric(df['odometer'])
 
 df_out = df
 df_out.to_csv('carmudi_data_cleaned.csv', index=False)
